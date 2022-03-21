fab64a70c04cc8aa6a5516932ff4f3588e8fe575
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-03-21 14:44:28 +1300

    Add files via upload
    Comments

---
+++ 00_MMF_Base_v1.py
@@ -0,0 +1,28 @@
+# import statements
+
+# functions go there
+
+#  ******* Main Routine *******
+
+# Set up dictionaries / lists needed to hold
+
+# Ask user if they have used the programme before & show instructions
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # ask for payment method (and apply surcharge if necessary)
+
+
+# Calculate Total sales and profit
+
+# Output data to text file
