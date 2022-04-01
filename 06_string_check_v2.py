2aa66195bf1a88be77d63afc654cee1a60d7e6b7
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-04-01 13:35:40 +1300

    Add files via upload
    Added in items. Ask what snack they want and if invalid answer is given the repeat question

---
+++ 06_string_check_v2.py
@@ -0,0 +1,72 @@
+
+# valid snacks holds list of all snacks
+# Each item in valid snacks is a list with
+# valid options for each snack <full name, letter code (a-e)
+# and possible abbreviations etc>
+valid_snacks = [
+    ["popcorn", "p", "corn", "a"],
+    ["M&M's", "m&m's", "mms", "m", "b"],   # first item is M&M
+    ["pita chips", "chips", "pc", "pita", "c"],
+    ["water", "w", "d"]
+]
+
+# initialise variables
+snack_ok = ""
+snack = ""
+
+# loop three times to make testing quicker
+for item in range(0, 3):
+    
+    # ask user for desired snack and put it in lowercase
+    desired_snack = input("Snack: ").lower()
+
+    for var_list in valid_snacks:
+
+        # if the snack is in one of the lists, return
+        if desired_snack in var_list:
+
+            # Get full name of snack and put it
+            # in title case, so it looks nice when output
+            snack = var_list[0].title()
+            snack_ok = "yes"
+            break
+
+        # if the chosen snack is not valid, set snack_ok to no
+        else:
+            snack_ok = "no"
+
+    # if the snack is not OK - ask question again.
+    if snack_ok == "yes":
+        print("Snack Choice: ", snack)
+    else:
+        print("invalid choice")
+
+
+
+def string_checker(question, to_check):
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        if response in to_check:
+            return response
+
+        else:
+            for item in to_check:
+                # check if response is the first letter of
+                # an item in the list
+                if response == item[0]:
+                    # note: return the entire response
+                    # rather than just the first letter
+                    return item
+
+        print("Sorry that is not a valid response")
+
+# *** Main routine starts here ***
+for item in range(0,6):
+    want_snacks = string_checker("Do you want "
+                                 "snacks? ", ["yes","no"])
+    print("Answer OK, you said:", want_snacks)
+    print()
