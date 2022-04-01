ae258827a966188f825bd24a58fb159e229104fe
maryann-map <101371469+maryann-map@users.noreply.github.com>
2022-04-01 14:01:11 +1300

    Add files via upload
    Added in functions. Loop asking user if they would like a snack and then ask what snacks they want.

---
+++ 06_string_check_v3.py
@@ -0,0 +1,55 @@
+
+
+# Function goes here
+def string_check(choice, options):
+
+    for var_list in options:
+
+        # if the snack is in one of the lists, return
+        if choice in var_list:
+
+            # Get full name of snack and put it
+            # in title case , so it looks nice when output
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+
+        # if the chosen option is not valid, set is_valid to no
+        else:
+            is_valid = "no"
+
+    # if the snack is not OK - ask question again.
+    if is_valid == "yes":
+        return chosen
+    else:
+        return "invalid choice"
+# valid snacks holds list of all snacks
+# Each item in valid snacks is a list with
+# valid options for each snack <full name, letter code
+# , and possible abbreviations etc.>
+valid_snacks = [
+    ["popcorn", "p", "corn", "a"],
+    ["M&M's", "m&m's", "mms", "m", "b"],   # first item is M&M
+    ["pita chips", "chips", "pc", "pita", "c"],
+    ["water", "w", "d"]
+]
+
+yes_no = [
+    ["yes", "y"],
+    ["no", "n"]
+]
+
+check_snack = "invalid choice"
+while check_snack == "invalid choice":
+    want_snack = input("Do you want to order snacks?")
+    check_snack = string_check(want_snack, yes_no)
+
+# loop six times to make testing quicker
+for item in range(0, 6):
+    
+    # ask user for desired snack and put it in lowercase
+    desired_snack = input("Snack: ").lower()
+
+    # check if snack is valid
+    snack_choice = string_check(desired_snack, valid_snacks)
+    print("Snack Choice: ", snack_choice)
